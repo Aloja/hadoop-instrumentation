5b1037d4ea53e8ed4b921c1ec8e9aeae0b68227f
jcugat <jcugat@minerva-users-1.ac.upc.es>
2015-01-28 18:20:16 +0100

    Fixed some paths in makefiles

--- vars-default.sh
+++ vars-default.sh
@@ -16,13 +16,20 @@ export PCUSER=smendoza
 export LNESS_HOME=${BASE_DIR}
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::" | sed "s:/jre::")
 export JAVA="${JAVA_HOME}/bin/java"
+export JAVAC="${JAVA_HOME}/bin/javac"
+export JAVAH="${JAVA_HOME}/bin/javah"
+export JAR="${JAVA_HOME}/bin/jar"
+export JAVA_INCLUDE="${JAVA_HOME}/include/linux"
+export JAVA_INCLUDE2="${JAVA_HOME}/include"
 export LNESS_LIBS=$LNESS_HOME/lib
 export LD_LIBRARY_PATH=$LNESS_HOME/lib
 export TMP_PPING=/tmp/smendoza
 export HADOOP_PREFIX="${BASE_DIR}/hadoop-build"
 export HADOOP_JAR_EXAMPLES="${HADOOP_PREFIX}/hadoop-examples-*.jar"
+export HADOOP_SRC="${BASE_DIR}/hadoop-src"
 
-export LOG4J_JAR="${LOCAL_DIR}/lib/log4j-1.2.17.jar"
+export JNI_H_DEST="${BASE_DIR}/extrae/java_wrapper/jni_c/include"
+
 export LOG4J_CONFFILE="${BASE_DIR}/log4j.undef2prv.properties"
 
 export CATALINA_HOME=/home/smendoza/lightness/hadoop-apps/apache-tomcat-6.0.4
