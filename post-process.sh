5ad89f1b4e62f3de482b2f8d7a0714bfb78f3321
Josep Cugat <jcugat@gmail.com>
2015-02-02 11:33:11 +0100

    Unified post-process in a single script

--- post-process.sh
+++ post-process.sh
@@ -5,9 +5,12 @@ set -o xtrace   # Debug mode: display the command and its expanded arguments
 
 . "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/vars.sh
 
-#############################################
-### GET THE HADOOP NODES DUMPING's ##########
-#############################################
+
+
+#############################
+### POST-PROCESS-DUMPINGD ###
+#############################
+
 #dumping: filtrado del volcado del lsof
 while read node
 do
@@ -37,3 +40,43 @@ cat $f >> $TMP_PPING/distributed-merge/dumping-host-port-pid
 done
 
 
+
+##################################
+### POST-PROCESS-EXTRAE-TRACES ###
+##################################
+
+#get extrae traces from hadoop nodes
+rm -f $TMP_PPING/distributed-merge/TRACE.mpits
+while read node
+do
+rm -rf $TMP_PPING/distributed-merge/$node
+mkdir -p $TMP_PPING/distributed-merge/$node
+scp -r $node:$TMP_PPING/set-* $TMP_PPING/distributed-merge/$node
+scp -r $node:$TMP_PPING/TRACE.mpits $TMP_PPING/distributed-merge/$node
+for f in $TMP_PPING/distributed-merge/$node/set-*/*mpit
+do
+echo $f on minerva-$node named>> $TMP_PPING/distributed-merge/TRACE.mpits
+echo "--" >> $TMP_PPING/distributed-merge/TRACE.mpits
+done
+done < $HADOOP_PREFIX/conf/slaves
+
+#quito el ultimo -- para que mpi2prv no me de error
+cp $TMP_PPING/distributed-merge/TRACE.mpits $TMP_PPING/distributed-merge/TRACE.mpits.tmp
+head -n -1 $TMP_PPING/distributed-merge/TRACE.mpits.tmp > $TMP_PPING/distributed-merge/TRACE.mpits
+rm $TMP_PPING/distributed-merge/TRACE.mpits.tmp
+
+#Generacion de todos los mpits con el TRACE.mpits separados por apps
+${BIN_DIR}/mpi2prv -no-syn -f $TMP_PPING/distributed-merge/TRACE.mpits -o $TMP_PPING/mergeoutput.prv
+
+
+
+##############################
+### POST-PROCESS-UNDEF2PRV ###
+##############################
+
+#clean log files
+rm -f $TMP_PPING/undef2prv.log*
+
+#execute the undef2prv post-processing
+#procesa els ports i els genera al output
+${JAVA} -cp "${LIB_DIR}/*" es.bsc.tools.undef2prv.Undef2prv $TMP_PPING/distributed-merge/dumping-host-port-pid $TMP_PPING/mergeoutput.prv $TMP_PPING/mergeoutput.row $TMP_PPING/mergeoutput.pcf
