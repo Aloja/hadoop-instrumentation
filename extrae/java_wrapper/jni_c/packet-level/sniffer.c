1ed290ab2995f3f5588e87b24dfadb01fae7a88a
Josep Cugat <jcugat@gmail.com>
2015-02-15 21:12:47 +0100

    Fixed sniffer not capturing all communications in the same host

--- extrae/java_wrapper/jni_c/packet-level/sniffer.c
+++ extrae/java_wrapper/jni_c/packet-level/sniffer.c
@@ -27,8 +27,12 @@ int sniffing(int inbound, int ports[], int nports) {
 	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_t* descr_receiving_in;
 	pcap_t* descr_receiving_out;
+	pcap_t* descr_receiving_local_in;
+	pcap_t* descr_receiving_local_out;
 	struct bpf_program fp_in; /* to hold compiled program */
 	struct bpf_program fp_out; /* to hold compiled program */
+	struct bpf_program fp_local_in; /* to hold compiled program */
+	struct bpf_program fp_local_out; /* to hold compiled program */
 	bpf_u_int32 pMask; /* subnet mask */
 	bpf_u_int32 pNet; /* ip address*/
 	pcap_if_t *alldevs, *d;
@@ -41,7 +45,9 @@ int sniffing(int inbound, int ports[], int nports) {
 	char p_npkts[] = "packets";
 	char *my_dev = calloc(100, sizeof (char));
 	char *tmp_cmaxp = calloc(100, sizeof (char));
-	char *filter = calloc(2000, sizeof (char));
+	char *filter_base = calloc(2000, sizeof (char));
+	char *filter_remote = calloc(2000, sizeof (char));
+	char *filter_local = calloc(2000, sizeof (char));
 	int max_packets = 1;
 
 	if (!inbound){
@@ -51,11 +57,15 @@ int sniffing(int inbound, int ports[], int nports) {
 
 	descr_receiving_in = pcap_create(NULL, errbuf);
 	descr_receiving_out = pcap_create(NULL, errbuf);
-	if (descr_receiving_in == NULL || descr_receiving_out == NULL)
+	descr_receiving_local_in = pcap_create(NULL, errbuf);
+	descr_receiving_local_out = pcap_create(NULL, errbuf);
+	if (descr_receiving_in == NULL || descr_receiving_out == NULL || descr_receiving_local_in == NULL || descr_receiving_local_out == NULL)
 		exit(-1);
 
 	pcap_set_buffer_size(descr_receiving_in, 524288);
 	pcap_set_buffer_size(descr_receiving_out, 524288);
+	pcap_set_buffer_size(descr_receiving_local_in, 524288);
+	pcap_set_buffer_size(descr_receiving_local_out, 524288);
 
 	if(pcap_activate(descr_receiving_in)!=0){
 		printf("pcap_activate(): %s\n", pcap_geterr(descr_receiving_in));
@@ -64,6 +74,16 @@ int sniffing(int inbound, int ports[], int nports) {
 	}
 	if(pcap_activate(descr_receiving_out)!=0){
                 printf("pcap_activate(): %s\n", pcap_geterr(descr_receiving_out));
+                printf("\npcap_activate(): LD_LIBRARY_PATH=%s\n", getenv("LD_LIBRARY_PATH"));
+		exit(-1);
+	}
+	if(pcap_activate(descr_receiving_local_in)!=0){
+		printf("pcap_activate(): %s\n", pcap_geterr(descr_receiving_local_in));
+                printf("\npcap_activate(): LD_LIBRARY_PATH=%s\n", getenv("LD_LIBRARY_PATH"));
+		exit(-1);
+	}
+	if(pcap_activate(descr_receiving_local_out)!=0){
+                printf("pcap_activate(): %s\n", pcap_geterr(descr_receiving_local_out));
                 printf("\npcap_activate(): LD_LIBRARY_PATH=%s\n", getenv("LD_LIBRARY_PATH"));
 		exit(-1);
 	}
@@ -82,26 +102,57 @@ int sniffing(int inbound, int ports[], int nports) {
 		pcap_perror(descr_receiving_out, error_prefix);
 		exit(-1);
 	}
+	int setdirection_local_in_result = pcap_setdirection(descr_receiving_local_in, PCAP_D_INOUT);
+	if (setdirection_local_in_result != 0) {
+		printf("pcap_setdirection in result: %d\n", setdirection_local_in_result);
+		pcap_perror(descr_receiving_local_in, error_prefix);
+		exit(-1);
+	}
+	int setdirection_local_out_result = pcap_setdirection(descr_receiving_local_out, PCAP_D_INOUT);
+	if (setdirection_local_out_result != 0) {
+		printf("pcap_setdirection out result: %d\n", setdirection_local_out_result);
+		pcap_perror(descr_receiving_local_out, error_prefix);
+		exit(-1);
+	}
 
 
 	//TODO: generate a filter with the ports specified by hadoop
 	//filter by ports and just unicast packets
-	generate_filter(filter, ports, nports);
+	generate_filter(filter_base, ports, nports);
 	//    strcpy( filter, "tcp" );
-	printf("generate_filter()=%s\n", filter);
+	printf("generate_filter()=%s\n", filter_base);
+	sprintf(filter_remote, "(%s) and (ip[12:4] != ip[16:4])", filter_base);
+	printf("filter_remote=%s\n", filter_remote);
+	sprintf(filter_local, "(%s) and (ip[12:4] == ip[16:4])", filter_base);
+	printf("filter_local=%s\n", filter_local);
 
 	// Compile the filter expression
-	if (pcap_compile(descr_receiving_in, &fp_in, filter, 0, pNet) == -1) {
+	if (pcap_compile(descr_receiving_in, &fp_in, filter_remote, 0, pNet) == -1) {
 		printf("\npcap_compile() failed\n");
-		printf("\npcap_compile(): filter used: %s\n", filter);
+		printf("\npcap_compile(): filter used: %s\n", filter_remote);
 		printf("pcap_compile(): %s\n", pcap_geterr(descr_receiving_in));
 		printf("\npcap_compile(): LD_LIBRARY_PATH=%s", getenv("LD_LIBRARY_PATH"));
 		return -1;
 	}
-	if (pcap_compile(descr_receiving_out, &fp_out, filter, 0, pNet) == -1) {
+	if (pcap_compile(descr_receiving_out, &fp_out, filter_remote, 0, pNet) == -1) {
 		printf("\npcap_compile() failed\n");
-		printf("\npcap_compile(): filter used: %s\n", filter);
+		printf("\npcap_compile(): filter used: %s\n", filter_remote);
 		printf("pcap_compile(): %s\n", pcap_geterr(descr_receiving_out));
+		printf("\npcap_compile(): LD_LIBRARY_PATH=%s", getenv("LD_LIBRARY_PATH"));
+		return -1;
+	}
+	if (pcap_compile(descr_receiving_local_in, &fp_local_in, filter_local, 0, pNet) == -1) {
+		printf("\npcap_compile() failed\n");
+		printf("\npcap_compile(): filter used: %s\n", filter_local);
+		printf("pcap_compile(): %s\n", pcap_geterr(descr_receiving_local_in));
+		printf("\npcap_compile(): LD_LIBRARY_PATH=%s", getenv("LD_LIBRARY_PATH"));
+		return -1;
+	}
+	if (pcap_compile(descr_receiving_local_out, &fp_local_out, filter_local, 0, pNet) == -1) {
+		printf("\npcap_compile() failed\n");
+		printf("\npcap_compile(): filter used: %s\n", filter_local);
+		printf("pcap_compile(): %s\n", pcap_geterr(descr_receiving_local_out));
+		printf("\npcap_compile(): LD_LIBRARY_PATH=%s", getenv("LD_LIBRARY_PATH"));
 		return -1;
 	}
 
@@ -114,6 +165,14 @@ int sniffing(int inbound, int ports[], int nports) {
 		printf("\npcap_setfilter() failed\n");
 		exit(1);
 	}
+	if (pcap_setfilter(descr_receiving_local_in, &fp_local_in) == -1) {
+		printf("\npcap_setfilter() failed\n");
+		exit(1);
+	}
+	if (pcap_setfilter(descr_receiving_local_out, &fp_local_out) == -1) {
+		printf("\npcap_setfilter() failed\n");
+		exit(1);
+	}
 	char in = (char) 1;
 	char out = (char) 0;
 	printf("sniffing on: %d\n", (int) in);
@@ -121,6 +180,8 @@ int sniffing(int inbound, int ports[], int nports) {
 	while (1) {
 		pcap_dispatch(descr_receiving_out, max_packets, callback, &out);
 		pcap_dispatch(descr_receiving_in, max_packets, callback, &in);
+		pcap_dispatch(descr_receiving_local_out, max_packets, callback, &out);
+		pcap_dispatch(descr_receiving_local_in, max_packets, callback, &in);
 	}
 	printf("dispatch finished, voy para el Extrae_fini()\n");
 	//Extrae_fini(); //Fini absurdo,pq está fuera del while
